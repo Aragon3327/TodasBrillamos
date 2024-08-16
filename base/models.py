c2dc17cc60e4c5cb0296c15c34b21373dc7acdda
Miguel Galicia <miguelg987d@gmail.com>
2024-08-16 15:21:47 -0600

    Base de datos

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Modelo de Categorías
+class Categorias(models.Model):
+    nombre = models.CharField(max_length=255)
+
+# Modelo de items.
+class Items(models.Model):
+    nombre = models.CharField(max_length=255)
+    stock = models.IntegerField()
+    precio = models.FloatField()
+    descuento = models.FloatField()
+    descripción = models.TextField()
+    categoria = models.ForeignKey(Categorias, on_delete=models.SET_NULL,null=True)
+    
